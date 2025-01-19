a1fb9a93af8ceeb07a686cbb5c3d9c3ca377724c
tadacheng <tang.dancheng@gmail.com>
2025-01-19 15:26:45 +0800

    Add Task

---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String getStatusIcon() {
+        return this.isDone ? "X" : " ";
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+
+}
