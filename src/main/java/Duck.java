76864cafbf3a61485ad7538513a377b9f82ad6c2
tadacheng <tang.dancheng@gmail.com>
2025-01-19 14:37:39 +0800

    Level 1. Echo

--- src/main/java/Duck.java
+++ src/main/java/Duck.java
@@ -1,12 +1,19 @@
+import java.util.Scanner;
+
 public class Duck {
     public static void main(String[] args) {
-        String greeting = "Hello! I'm Duck\nWhat can I do for you?";
-        String goodbye = "Bye. Hope to see you again soon!";
-        String divider = "____________________________________________________________";
-        System.out.println(divider);
+        String divider = "____________________________________________________________\n";
+        String greeting =  divider + "Hello! I'm Duck\nWhat can I do for you?\n" + divider;
+        String goodbye = divider + "Bye. Hope to see you again soon!\n" + divider;
+
+        Scanner sc = new Scanner(System.in);
         System.out.println(greeting);
-        System.out.println(divider);
+        String user_input;
+        while (true) {
+            user_input = sc.nextLine();
+            if (user_input.equals("bye")) break;
+            System.out.println(divider + user_input + "\n" + divider);
+        }
         System.out.println(goodbye);
-        System.out.println(divider);
     }
 }
