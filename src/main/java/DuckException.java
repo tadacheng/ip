2f187fe0a969694a2ecb18e68b6a5ab896658c82
tadacheng <tang.dancheng@gmail.com>
2025-01-19 17:26:25 +0800

    Added DuckException

---
+++ src/main/java/DuckException.java
@@ -0,0 +1,7 @@
+public class DuckException extends Exception{
+    public DuckException(String message) {
+        super("____________________________________________________________\nError: " +
+                message +
+                "\n____________________________________________________________\n");
+    }
+}
