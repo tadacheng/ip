d4252dffc6153d02d437b102ebc60a0480c92ecd
tadacheng <tang.dancheng@gmail.com>
2025-01-19 14:23:49 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Duck.java
@@ -0,0 +1,12 @@
+public class Duck {
+    public static void main(String[] args) {
+        String greeting = "Hello! I'm Duck\nWhat can I do for you?";
+        String goodbye = "Bye. Hope to see you again soon!";
+        String divider = "____________________________________________________________";
+        System.out.println(divider);
+        System.out.println(greeting);
+        System.out.println(divider);
+        System.out.println(goodbye);
+        System.out.println(divider);
+    }
+}
