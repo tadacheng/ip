9265b7d7d9b0e072535a77b310092a960eae38ce
tadacheng <tang.dancheng@gmail.com>
2025-01-19 15:26:57 +0800

    Level 3. Mark as Done

--- src/main/java/Duck.java
+++ src/main/java/Duck.java
@@ -1,36 +1,53 @@
+import java.io.*;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Duck {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String divider = "____________________________________________________________\n";
         String greeting = divider + "Hello! I'm Duck\nWhat can I do for you?\n" + divider;
         String goodbye = divider + "Bye. Hope to see you again soon!\n" + divider;
 
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println(greeting);
-        String user_input;
-        ArrayList<String> tasks = new ArrayList<>();
+        String[] user_input;
+        ArrayList<Task> tasks_list = new ArrayList<>();
 
         while (true) {
-            user_input = sc.nextLine().trim();
-            if (user_input.equals("bye")) {
+            user_input = br.readLine().split(" ");
+            if (user_input[0].equals("bye")) {
                 System.out.println(goodbye);
                 break;
-            } else if (user_input.equals("list")) {
+            } else if (user_input[0].equals("list")) {
                 System.out.print(divider);
-                if (tasks.isEmpty()) {
+                if (tasks_list.isEmpty()) {
                     System.out.println("No tasks in the list.");
                 } else {
-                    for (int i = 0; i < tasks.size(); i++) {
-                        System.out.println((i + 1) + ". " + tasks.get(i));
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < tasks_list.size(); i++) {
+                        Task task = tasks_list.get(i);
+                        System.out.println((i + 1) + ".[" + task.getStatusIcon() + "] " + task.getDescription());
                     }
                 }
                 System.out.print(divider);
+            } else if (user_input[0].equals("mark")) {
+
+                Task task = tasks_list.get(Integer.parseInt(user_input[1]) - 1);
+                task.markAsDone();
+                System.out.println(divider + "Nice! I've marked this task as done:");
+                System.out.println("  [" + task.getStatusIcon() + "] " + task.getDescription());
+                System.out.print(divider);
+            } else if (user_input[0].equals("unmark")) {
+                Task task = tasks_list.get(Integer.parseInt(user_input[1]) - 1);
+                task.markAsNotDone();
+                System.out.println(divider + "OK, I've marked this task as not done yet:");
+                System.out.println("  [" + task.getStatusIcon() + "] " + task.getDescription());
+                System.out.print(divider);
             } else {
-                tasks.add(user_input);
-                System.out.println(divider + "added: " + user_input + "\n" + divider);
+                tasks_list.add(new Task(String.join(" ", user_input)));
+                System.out.println(divider + "added: " + String.join(" ", user_input) + "\n" + divider);
             }
+
         }
+        br.close();
     }
 }
