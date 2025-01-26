d2754b1c322d7780684da763cd6ecb6ca74103cc
tadacheng <tang.dancheng@gmail.com>
2025-01-26 15:31:26 +0800

    Change dates in Deadline and Event from String to LocalDateTime

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,16 +1,30 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    protected String from;
-    protected String to;
+    protected LocalDateTime from;
+    protected LocalDateTime to;
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("MMM dd yyy, hh:mm a");
 
     public Event(String description, String from, String to) {
+        super(description);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+        this.from = LocalDateTime.parse(from, formatter);
+        this.to = LocalDateTime.parse(to, formatter);
+    }
+
+    public Event(String description, LocalDateTime from, LocalDateTime to) {
         super(description);
         this.from = from;
         this.to = to;
     }
 
+
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + this.from + " to: " + this.to + ")";
+        return "[E]" + super.toString() + " (from: " + this.from.format(DATE_TIME_FORMATTER)
+                + " to: " + this.to.format(DATE_TIME_FORMATTER) + ")";
     }
 
     @Override
