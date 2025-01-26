c5688dd132f66ffac525c5be094b6da232d5b536
tadacheng <tang.dancheng@gmail.com>
2025-01-26 17:07:05 +0800

    Move Tasklist operations to separate class

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskList {
+    private final List<Task> tasks;
+
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+    public TaskList(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public void addTask(Task task) {
+        this.tasks.add(task);
+    }
+
+    public Task deleteTask(int id) throws DuckException {
+        if (id < 0 || id >= this.tasks.size()) {
+            throw new DuckException("Invalid task number. Use list to view task id");
+        }
+        return tasks.remove(id);
+    }
+
+    public Task getTask(int id) {
+        return tasks.get(id);
+    }
+    public List<Task> getAllTasks() {
+        return tasks;
+    }
+
+    public int size() {
+        return tasks.size();
+    }
+}
